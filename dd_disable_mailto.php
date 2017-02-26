772d1de0681394de7062fa98f0aa049fd84bd566
Florian Häusler <florian@didldu.com>
2017-02-26 15:07:24 +0100

    Revision

--- dd_disable_mailto.php
+++ dd_disable_mailto.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version    1-2-2-0 // Y-m-d 2016-10-04
+ * @version    1-2-2-0 // Y-m-d 2017-02-26
  * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
  * @copyright  Copyright (C) 2011 - 2016 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only

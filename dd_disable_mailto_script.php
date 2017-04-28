196ad821c48174f872df92bc07380b70935f5712
Florian Häusler <florian@didldu.com>
2017-04-28 16:45:58 +0200

    New legal notes / copyright header

--- dd_disable_mailto_script.php
+++ dd_disable_mailto_script.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * @version    1-1-0-0 // Y-m-d 2016-10-01
- * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
- * @copyright  Copyright (C) 2011 - 2016 Didldu e.K. | HR IT-Solutions
+ * @package    DD_Disable_MailTo
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  **/
 
@@ -12,16 +13,17 @@ class plgSystemDD_Disable_MailToInstallerScript
 {
 	/**
 	 * Reset Joomla default settings
-	 * @since Version 3.6.2
+	 *
+	 * @since Version 1.0.0.0
 	 */
 	public function uninstall()
 	{
 		$db  = JFactory::getDbo();
 		$query = $db->getQuery(true);
 		$query->update('#__extensions')
-			->set($db->quoteName('enabled') . ' = 1')
-			->where($db->quoteName('element') . ' = ' . $db->quote('com_mailto'))
-			->where($db->quoteName('type') . ' = ' . $db->quote('component'));
+			->set($db->qn('enabled') . ' = 1')
+			->where($db->qn('element') . ' = ' . $db->q('com_mailto'))
+			->where($db->qn('type') . ' = ' . $db->q('component'));
 		$db->setQuery($query);
 		$db->execute();
 	}
